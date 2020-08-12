57654122c679a8d96e35f3dbae8189966686242b
1like2learn <ndespained@gmail.com>
2020-08-11 17:54:39 -0700

    moved work from previous directory into work project

---
+++ Orders/src/main/java/com/lambdaschool/orders/models/Customer.java
@@ -0,0 +1,208 @@
+package com.lambdaschool.orders.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "CUSTOMERS")
+public class Customer {
+
+    //dim primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long custcode;
+
+    //dim name
+    private String custname;
+
+    //dim city
+    private String custcity;
+
+    //dim working area
+    private String workingarea;
+
+    //dim country
+    private String custcountry;
+
+    //dim grade
+    private String grade;
+
+    //dim opening amount
+    private double  openingamt;
+
+    //dim receive amount
+    private double receiveamt;
+
+    //dim payment amount
+    private double paymentamt;
+
+    //dim outstanding amount
+    private double outstandingamt;
+
+    //dim phone
+    private String phone;
+
+    //connect the customers table to the agents table in a many to one relation
+    @ManyToOne
+    @JoinColumn(name = "agentcode", nullable = false)
+    @JsonIgnoreProperties("customers")
+    private Agent agent;
+
+    //connect customers to orders table in a one to many relation
+    @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonIgnoreProperties("customer")
+    List<Order> orders = new ArrayList<>();
+
+
+    //Constructors
+    public Customer() {
+    }
+
+    public Customer(
+        String custname,
+        String custcity,
+        String workingarea,
+        String custcountry,
+        String grade,
+        double openingamt,
+        double receiveamt,
+        double paymentamt,
+        double outstandingamt,
+        String phone,
+        Agent agent) {
+        this.custname = custname;
+        this.custcity = custcity;
+        this.workingarea = workingarea;
+        this.custcountry = custcountry;
+        this.grade = grade;
+        this.openingamt = openingamt;
+        this.receiveamt = receiveamt;
+        this.paymentamt = paymentamt;
+        this.outstandingamt = outstandingamt;
+        this.phone = phone;
+        this.agent = agent;
+    }
+
+    /*=== getters and setters ===*/
+
+    //customer code
+    public long getCustcode() {
+        return custcode;
+    }
+
+    public void setCustcode(long custcode) {
+        this.custcode = custcode;
+    }
+
+    //customer name
+    public String getCustname() {
+        return custname;
+    }
+
+    public void setCustname(String custname) {
+        this.custname = custname;
+    }
+
+    //customer city
+    public String getCustcity() {
+        return custcity;
+    }
+
+    public void setCustcity(String custcity) {
+        this.custcity = custcity;
+    }
+
+    //customer working area
+    public String getWorkingarea() {
+        return workingarea;
+    }
+
+    public void setWorkingarea(String workingarea) {
+        this.workingarea = workingarea;
+    }
+
+    //customer country
+    public String getCustcountry() {
+        return custcountry;
+    }
+
+    public void setCustcountry(String custcountry) {
+        this.custcountry = custcountry;
+    }
+
+    //grade
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    //opening amount
+    public double getOpeningamt() {
+        return openingamt;
+    }
+
+    public void setOpeningamt(double openingamt) {
+        this.openingamt = openingamt;
+    }
+
+    //recieving amount
+    public double getReceiveamt() {
+        return receiveamt;
+    }
+
+    public void setReceiveamt(double receiveamt) {
+        this.receiveamt = receiveamt;
+    }
+
+    //payment amount
+    public double getPaymentamt() {
+        return paymentamt;
+    }
+
+    public void setPaymentamt(double paymentamt) {
+        this.paymentamt = paymentamt;
+    }
+
+    //outstanding amount
+    public double getOutstandingamt() {
+        return outstandingamt;
+    }
+
+    public void setOutstandingamt(double outstandingamt) {
+        this.outstandingamt = outstandingamt;
+    }
+
+    //phone
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    //agent
+    public Agent getAgent() {
+        return agent;
+    }
+
+    public void setAgent(Agent agent) {
+        this.agent = agent;
+    }
+
+    //orders
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
+
+    private List<Order> getOrders() {
+        return orders;
+    }
+}
---
+++ Orders/src/main/java/com/lambdaschool/orders/models/Order.java
@@ -0,0 +1,109 @@
+package com.lambdaschool.orders.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    //dim order number as the primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false)
+    private long ordnum;
+
+    //dim order amount
+    private double ordamount;
+
+    //dim advance amount
+    private double advanceamount;
+
+    //dim order description
+    private String orderdescription;
+
+    @ManyToOne
+    @JoinColumn(name = "custcode", nullable = false)
+    @JsonIgnoreProperties("orders")
+    private Customer customer;
+
+    @ManyToMany()
+    @JoinTable(name = "orderspayments",
+    joinColumns = @JoinColumn(name = "ordnum"),
+    inverseJoinColumns = @JoinColumn(name = "paymentid"))
+    @JsonIgnoreProperties("orders")
+    Set<Payment> payments = new HashSet<>();
+
+    //constructors
+    public Order() {
+    }
+
+    public Order(
+        double ordamount,
+        double advanceamount,
+        Customer customer,
+        String orderdescription) {
+        this.ordamount = ordamount;
+        this.customer = customer;
+        this.advanceamount = advanceamount;
+        this.orderdescription = orderdescription;
+        this.payments = getPayments();
+    }
+
+    /*=== getters and setters ===*/
+
+    public long getOrdnum() {
+        return ordnum;
+    }
+
+    public void setOrdnum(long ordnum) {
+        this.ordnum = ordnum;
+    }
+
+    public double getOrdamount() {
+        return ordamount;
+    }
+
+    public void setOrdamount(double ordamount) {
+        this.ordamount = ordamount;
+    }
+
+    public double getAdvanceamount() {
+        return advanceamount;
+    }
+
+    public void setAdvanceamount(double advanceamount) {
+        this.advanceamount = advanceamount;
+    }
+
+    public String getOrderdescription() {
+        return orderdescription;
+    }
+
+    public void setOrderdescription(String orderdescription) {
+        this.orderdescription = orderdescription;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Set<Payment> getPayments() {
+        return payments;
+    }
+
+    public void setPayments(Set<Payment> payments) {
+        this.payments = payments;
+    }
+
+    public void addPayment(Payment payment){
+        this.payments.add(payment);
+    }
+}
